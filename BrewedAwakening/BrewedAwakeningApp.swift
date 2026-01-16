b49a1ce9e4d0acc158f346a06382b47cd9ffbd15
Lily <lmakula6243@stu.d214.org>
2026-01-16 11:10:44 -0600

    Initial Commit

---
+++ BrewedAwakening/BrewedAwakeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewedAwakeningApp.swift
+//  BrewedAwakening
+//
+//  Created by Lily P. Makula on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct BrewedAwakeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewedAwakening/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrewedAwakening
+//
+//  Created by Lily P. Makula on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
