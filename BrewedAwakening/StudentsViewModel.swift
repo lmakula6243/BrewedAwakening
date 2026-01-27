79671a37d5e552cc0224d040a9ff05d0d9e42b7f
Ola <Aoleksiak8215@stu.d214.org>
2026-01-27 10:27:02 -0600

    added function but commented it out

--- BrewedAwakening/StudentsViewModel.swift
+++ BrewedAwakening/StudentsViewModel.swift
@@ -7,16 +7,40 @@
 
 import Foundation
 
-
+//
+//import FirebaseDatabase
+//
 //
 //class StudentsViewModel: ObservableObject{
-//    @Published var students: [Students] = []
+//    @Published var students: [Student] = []
 //    
 //    init() {
 //        getData()
 //    }
-//               
+//    
 //    func getData(){
+//        let ref = Database.database().reference()
 //        
+//        ref.child("students").observe(.value) { snapshot in
+//            var tempArray: [Student] = []
+//            
+//            for child in snapshot.children {
+//                guard let snap = child as? DataSnapshot,
+//                      let dict = snap.value as? [String: Any] else { continue }
+//                
+//                let firstName = dict["firstName"] as? String ?? ""
+//                let lastName = dict["lastName"] as? String ?? ""
+//                let id = dict["id"] as? String ?? ""
+//                let scannerId = dict["scannerId"] as? String ?? ""
+//                
+//                tempArray.append(Student(firstName: firstName, lastName: lastName, id: id, scannerId: scannerId))
+//            }
+//            
+//            DispatchQueue.main.async {
+//                self.students = tempArray
+//            }
+//        }
 //    }
 //}
+    
+    
