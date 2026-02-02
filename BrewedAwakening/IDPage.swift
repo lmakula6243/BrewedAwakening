09e0dd03d1ff4becf779f0aa2e3e032768bb268b
Lily <lmakula6243@stu.d214.org>
2026-02-02 10:38:41 -0600

    fixed the ID page

---
+++ BrewedAwakening/IDPage.swift
@@ -0,0 +1,15 @@
+//
+//  IDPage.swift
+//  BrewedAwakening
+//
+//  Created by Lily P. Makula on 2/2/26.
+//
+
+import SwiftUI
+import FirebaseCore
+
+struct IDPage: View {
+    var body: some View {
+        Text("ID Page")
+    }
+}
--- BrewedAwakening/IDScannerChoicePage.swift
+++ BrewedAwakening/IDScannerChoicePage.swift
@@ -7,54 +7,76 @@ struct IDScannerChoicePage: View {
     @State var showIDSheet = false
     var body: some View {
         
-        HStack{
-            Button(action: {
-                showScanSheet.toggle()
-            }, label: {
-                
-                Text("Scanning Button: image of barcode")
-                Image("Scanner")
-                        })
-                    }
-        .sheet(isPresented: $showScanSheet){
-            HStack{
-                Text("Scan Student ID")
-                    .font(.title)
-                
-                TextField("Waiting for scan…", text: $scannedCode)
-                    .textFieldStyle(.roundedBorder)
-                
-                    .onSubmit {
-                        processScan(scannedCode)
-                        scannedCode = ""
-                        showScanSheet = false
-                    }
+        VStack {
+            NavigationStack{
+                NavigationLink {
+                    IDPage()
+                } label: {
+                    Image("Unknown 2 ")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+
             }
         }
-            Button(action: {
-                showIDSheet.toggle()
-            }, label: {
-                Text("ID Button: image of numberPAD")
-                Image("KeypadIcon")
-                        })
-                    
-        .sheet(isPresented: $showIDSheet){
-            Text("Scan Student ID")
-                .font(.title)
-            
-            TextField("Type Student ID here…", text: $typedID)
-                .textFieldStyle(.roundedBorder)
-            
-                .onSubmit {
-                    processID(typedID)
-                    typedID = ""
-                }
-                    }
-    }
-    func processScan(_ code: String){
-        print("Scanned: \(code)")
-    }
-    func processID(_ code: String){
-        print("Scanned: \(code)")
+        
+        
+        
+//        HStack{
+//            Button(action: {
+//                showScanSheet.toggle()
+//            }, label: {
+//                VStack {
+//                    Image("Unknown 2 ")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 60, height: 60)
+//                    Text("Scanning Button: image of barcode")
+//                    
+//                }
+//                        })
+//                    }
+//        .sheet(isPresented: $showScanSheet){
+//            HStack{
+//                Text("Scan Student ID")
+//                    .font(.title)
+//                
+//                TextField("Waiting for scan…", text: $scannedCode)
+//                    .textFieldStyle(.roundedBorder)
+//                
+//                    .onSubmit {
+//                        processScan(scannedCode)
+//                        scannedCode = ""
+//                        showScanSheet = false
+//                    }
+//            }
+//        }
+//            Button(action: {
+//                showIDSheet.toggle()
+//            }, label: {
+//                VStack {
+//                    Text("ID Button: image of numberPAD")
+//                    Image("KeypadIcon")
+//                }
+//                        })
+//                    
+//        .sheet(isPresented: $showIDSheet){
+//            Text("Scan Student ID")
+//                .font(.title)
+//            
+//            TextField("Type Student ID here…", text: $typedID)
+//                .textFieldStyle(.roundedBorder)
+//            
+//                .onSubmit {
+//                    processID(typedID)
+//                    typedID = ""
+//                }
+//                    }
+//    }
+//    func processScan(_ code: String){
+//        print("Scanned: \(code)")
+//    }
+//    func processID(_ code: String){
+//        print("Scanned: \(code)")
     }
 }
