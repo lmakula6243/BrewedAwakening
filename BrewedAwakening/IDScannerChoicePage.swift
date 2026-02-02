0558126aa643844de49b557dad03df10190cbbef
Ola <Aoleksiak8215@stu.d214.org>
2026-02-02 10:39:14 -0600

    added the code to show

--- BrewedAwakening/IDScannerChoicePage.swift
+++ BrewedAwakening/IDScannerChoicePage.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import FirebaseDatabase
 import FirebaseCore
 struct IDScannerChoicePage: View {
     @State var scannedCode = ""
@@ -6,55 +7,65 @@ struct IDScannerChoicePage: View {
     @State var typedID = ""
     @State var showIDSheet = false
     var body: some View {
-        
+        @StateObject var myViewModel: StudentsViewModel = StudentsViewModel()
         HStack{
-            Button(action: {
-                showScanSheet.toggle()
-            }, label: {
-                
-                Text("Scanning Button: image of barcode")
-                Image("Scanner")
-                        })
+            //delete later
+            List(){
+                List {
+                    ForEach(myViewModel.students) { student in
+                        Text("\(student.firstname)")
                     }
-        .sheet(isPresented: $showScanSheet){
-            HStack{
-                Text("Scan Student ID")
-                    .font(.title)
-                
-                TextField("Waiting for scan…", text: $scannedCode)
-                    .textFieldStyle(.roundedBorder)
-                
-                    .onSubmit {
-                        processScan(scannedCode)
-                        scannedCode = ""
-                        showScanSheet = false
-                    }
+                }
+                // delete later
+                Button(action: {
+                    showScanSheet.toggle()
+                }, label: {
+                    
+                    Text("Scanning Button: image of barcode")
+                    Image("Scanner")
+                })
             }
-        }
+            .sheet(isPresented: $showScanSheet){
+                HStack{
+                    Text("Scan Student ID")
+                        .font(.title)
+                    
+                    TextField("Waiting for scan…", text: $scannedCode)
+                        .textFieldStyle(.roundedBorder)
+                    
+                        .onSubmit {
+                            processScan(scannedCode)
+                            scannedCode = ""
+                            showScanSheet = false
+                        }
+                }
+            }
             Button(action: {
                 showIDSheet.toggle()
             }, label: {
                 Text("ID Button: image of numberPAD")
                 Image("KeypadIcon")
-                        })
-                    
-        .sheet(isPresented: $showIDSheet){
-            Text("Scan Student ID")
-                .font(.title)
+            })
             
-            TextField("Type Student ID here…", text: $typedID)
-                .textFieldStyle(.roundedBorder)
-            
-                .onSubmit {
-                    processID(typedID)
-                    typedID = ""
-                }
+            .sheet(isPresented: $showIDSheet){
+                Text("Scan Student ID")
+                    .font(.title)
+                
+                TextField("Type Student ID here…", text: $typedID)
+                    .textFieldStyle(.roundedBorder)
+                
+                    .onSubmit {
+                        processID(typedID)
+                        typedID = ""
                     }
+            }
+        }
     }
-    func processScan(_ code: String){
-        print("Scanned: \(code)")
+        func processScan(_ code: String){
+            print("Scanned: \(code)")
+        }
+        func processID(_ code: String){
+            print("Scanned: \(code)")
+        }
     }
-    func processID(_ code: String){
-        print("Scanned: \(code)")
-    }
-}
+
