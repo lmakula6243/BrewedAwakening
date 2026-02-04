b7969e47a61e38be5d0311da6bc3032c79325df0
Ola <Aoleksiak8215@stu.d214.org>
2026-02-04 11:08:07 -0600

    fixed formating

--- BrewedAwakening/IDScannerChoicePage.swift
+++ BrewedAwakening/IDScannerChoicePage.swift
@@ -17,8 +17,8 @@ struct IDScannerChoicePage: View {
                         Text(student.firstname)
                         Text(student.lastname)
                         Text(student.skey)
-                       Text("\(student.scannerId)")
-                        Text("\(student.id)")
+                        Text("Scanner ID: \(String(student.scannerId))")
+                        Text("Student ID: \(String(student.id))")
                     }
                 }
             }
