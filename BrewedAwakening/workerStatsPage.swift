eb04441637032d37a05e86db4b3ce3f62d313a39
Cassandra <cbotnari8050@stu.d214.org>
2026-02-23 11:00:38 -0600

    made the place where people can do different things. "menu"

--- BrewedAwakening/workerStatsPage.swift
+++ BrewedAwakening/workerStatsPage.swift
@@ -9,13 +9,52 @@ import SwiftUI
 import FirebaseCore
 
 struct workerStatsPage: View {
-    
+    @Binding var groups: [Group]
+    @State var selectedMenu: String = "Groups"
     var body: some View {
+        
         NavigationSplitView {
-            Text("Buttons go here")
-            
+            Text("MENU")
+                .font(.largeTitle)
+            List {
+                           Button("Groups") {
+                               selectedMenu = "Groups"
+                           }
+
+                           Button("Students") {
+                               selectedMenu = "Students"
+                           }
+
+                           Button("Calculate Pay") {
+                               selectedMenu = "Pay"
+                           }
+                       }
+                       .navigationTitle("Menu")
+
         } detail: {
-            Text("List Goes Here")
+
+            if selectedMenu == "Groups" {
+
+                            List(groups) { group in
+                                Text(group.groupName)
+                            }
+                            .navigationTitle("Groups")
+
+                        } else if selectedMenu == "Students" {
+                            Text("students search here")
+
+                        } else if selectedMenu == "Pay" {
+
+                            Text("Pay calculation goes here")
+                                .font(.largeTitle)
+                                .navigationTitle("Calculate Pay")
+
+                        } else if selectedMenu == "Groups"{
+                            Text("Groups go here")
+                        }else {
+
+                            Text("Select a menu option")
+                        }
         }
     }
     
